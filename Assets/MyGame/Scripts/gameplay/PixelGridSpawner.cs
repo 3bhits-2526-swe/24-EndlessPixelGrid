97333fbc96a68332cf74837450ffce9f2beaf4d6
Dave <david.unterberger08@htl-salzburg.ac.at>
2026-01-30 12:31:46 +0100

    Added 10x7 pixel grid using UI Images

--- Assets/MyGame/Scripts/gameplay/PixelGridSpawner.cs
+++ Assets/MyGame/Scripts/gameplay/PixelGridSpawner.cs
@@ -1,19 +1,38 @@
-using TMPro;
 using UnityEngine;
 
 public class PixelGridSpawner : MonoBehaviour
 {
     [SerializeField] private GameObject pixelPrefab;
     [SerializeField] private float gap = 5;
-    [SerializeField] private float yStartPos = 275;
+    [SerializeField] private float yStart = 5;
+    private int columns = 7;
+    private int rows = 10;
 
     private void Awake()
     {
-        gap = gap + 25;
-        for (int i = 0; i < 7; i++)
+        RectTransform rt = pixelPrefab.GetComponent<RectTransform>();
+        float cellWidth = rt.rect.width;
+        float cellHeight = rt.rect.height;
+
+        float totalWidth = (columns * cellWidth) + ((columns - 1) * gap);
+        float totalHeight = (rows * cellHeight) + ((rows - 1) * gap);
+
+        float startX = (-totalWidth / 2f) + (cellWidth / 2f);
+        float startY = yStart + (totalHeight / 2f) - (cellHeight / 2f);
+
+        for (int i = 0; i < columns; i++)
         {
-            for (int j = 0; j < 10; j++)
-                Instantiate(pixelPrefab, new Vector3(171 + i * gap, yStartPos + j * -gap, 0), Quaternion.identity, transform);
+            for (int j = 0; j < rows; j++)
+            {
+                GameObject pixel = Instantiate(pixelPrefab, transform);
+                RectTransform pixelRT = pixel.GetComponent<RectTransform>();
+
+                // Position relative to the parent center
+                float xPos = startX + (i * (cellWidth + gap));
+                float yPos = startY - (j * (cellHeight + gap));
+
+                pixelRT.anchoredPosition = new Vector2(xPos, yPos);
+            }
         }
     }
 }
